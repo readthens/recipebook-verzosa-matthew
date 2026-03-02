55c359a407a0b0e8359855a8a11e6faa5b95e3fc
readthens <matthewverzosa33@gmail.com>
2026-03-02 18:32:45 +0800

    feat(lab3): implement user-managed recipe auth flow and lecture-style admin setup

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import Profile, Recipe, RecipeIngredient
+from .models import Ingredient, Profile, Recipe, RecipeIngredient
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    extra = 0
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -15,5 +27,13 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
 
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Profile)
+admin.site.register(Ingredient, IngredientAdmin)
--- ledger/tests.py
+++ ledger/tests.py
@@ -1,15 +1,32 @@
 from django.contrib.admin.sites import site
+from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.test import TestCase
 from django.urls import reverse
 
 from .admin import RecipeAdmin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Ingredient, Profile, Recipe, RecipeIngredient
+
+
+def create_profile(username: str = "author") -> Profile:
+    user = User.objects.create_user(
+        username=username,
+        password="strong-pass-123",
+        email=f"{username}@example.com",
+    )
+    return Profile.objects.create(
+        user=user,
+        name=username.title(),
+        short_bio="A" * 256,
+    )
 
 
 class RecipeModelTests(TestCase):
+    def setUp(self):
+        self.profile = create_profile()
+
     def test_recipe_str_returns_name(self):
-        recipe = Recipe.objects.create(name="Sinigang")
+        recipe = Recipe.objects.create(name="Sinigang", author=self.profile)
         self.assertEqual(str(recipe), "Sinigang")
 
     def test_ingredient_str_returns_name(self):
@@ -17,11 +34,11 @@ class RecipeModelTests(TestCase):
         self.assertEqual(str(ingredient), "Tomato")
 
     def test_recipe_get_absolute_url_returns_detail_path(self):
-        recipe = Recipe.objects.create(name="Adobo")
+        recipe = Recipe.objects.create(name="Adobo", author=self.profile)
         self.assertEqual(recipe.get_absolute_url(), reverse("recipe_detail", kwargs={"pk": recipe.pk}))
 
     def test_recipeingredient_relations_accessible_via_related_names(self):
-        recipe = Recipe.objects.create(name="Sinigang")
+        recipe = Recipe.objects.create(name="Sinigang", author=self.profile)
         ingredient = Ingredient.objects.create(name="Pork")
         recipe_ingredient = RecipeIngredient.objects.create(
             recipe=recipe,
@@ -35,7 +52,9 @@ class RecipeModelTests(TestCase):
 
 class RecipeViewTests(TestCase):
     def setUp(self):
-        self.recipe = Recipe.objects.create(name="Sinigang")
+        self.profile = create_profile("viewer")
+        self.user = self.profile.user
+        self.recipe = Recipe.objects.create(name="Sinigang", author=self.profile)
         self.ingredient = Ingredient.objects.create(name="Tomato")
         self.recipe_ingredient = RecipeIngredient.objects.create(
             recipe=self.recipe,
@@ -48,22 +67,32 @@ class RecipeViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Sinigang")
 
-    def test_recipe_detail_returns_200_for_existing_pk(self):
+    def test_recipe_detail_redirects_to_login_when_unauthenticated(self):
+        detail_url = reverse("recipe_detail", kwargs={"pk": self.recipe.pk})
+        response = self.client.get(detail_url)
+        self.assertRedirects(response, f"{reverse('login')}?next={detail_url}")
+
+    def test_recipe_detail_returns_200_for_existing_pk_when_authenticated(self):
+        self.client.login(username=self.user.username, password="strong-pass-123")
         response = self.client.get(reverse("recipe_detail", kwargs={"pk": self.recipe.pk}))
         self.assertEqual(response.status_code, 200)
 
-    def test_recipe_detail_returns_404_for_missing_pk(self):
+    def test_recipe_detail_returns_404_for_missing_pk_when_authenticated(self):
+        self.client.login(username=self.user.username, password="strong-pass-123")
         response = self.client.get(reverse("recipe_detail", kwargs={"pk": 9999}))
         self.assertEqual(response.status_code, 404)
 
-    def test_recipe_detail_context_contains_related_ingredients(self):
+    def test_recipe_detail_context_contains_related_ingredients_when_authenticated(self):
+        self.client.login(username=self.user.username, password="strong-pass-123")
         response = self.client.get(reverse("recipe_detail", kwargs={"pk": self.recipe.pk}))
         self.assertEqual(response.status_code, 200)
         self.assertIn(self.recipe_ingredient, response.context["recipe"].ingredients.all())
 
-    def test_rendered_detail_shows_quantity_and_ingredient_name(self):
+    def test_rendered_detail_shows_quantity_ingredient_and_author_when_authenticated(self):
+        self.client.login(username=self.user.username, password="strong-pass-123")
         response = self.client.get(reverse("recipe_detail", kwargs={"pk": self.recipe.pk}))
         self.assertContains(response, "3pcs Tomato")
+        self.assertContains(response, self.profile.name)
 
 
 class FixtureAndAdminTests(TestCase):
